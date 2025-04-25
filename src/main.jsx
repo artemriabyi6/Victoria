b1d2a4fec8fa020093b0cbda69594cbfb798ae48
artemriabyi6 <ar04082001@gmail,com>
2025-04-25 16:23:09 +0300

    fourth deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,69 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements
+} from 'react-router-dom';
 import './i18n';
-import './index.css'
+import './index.css';
+
 import Calendar from './pages/calendar/Calendar.jsx';
 import HomePage from './pages/HomePage.jsx';
-import Team from './pages/team/Team.jsx'
+import Team from './pages/team/Team.jsx';
 import Stuff from './pages/stuff/Stuff.jsx';
 import PlayerPage from './pages/playerPage/PlayerPage.jsx';
 import History from './pages/history/History.jsx';
 import Photos from './pages/photos/Photos.jsx';
 import Table from './pages/table/Table.jsx';
 
-
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/calendar" element={<Calendar />} />
+      <Route path="/team" element={<Team />} />
+      <Route path="/stuff" element={<Stuff />} />
+      <Route path="/player/:id" element={<PlayerPage />} />
+      <Route path="/stuff/:id" element={<PlayerPage />} />
+      <Route path="/history" element={<History />} />
+      <Route path="/photos" element={<Photos />} />
+      <Route path="/table" element={<Table />} />
+      <Route path="/videos" element={<Photos />} />
+    </>
+  ),
   {
-    path: '/',
-    element: <HomePage/>,
-  },
-  {
-    path: '/calendar',
-    element: <Calendar/>
-  },
-  {
-    path: "/team",
-    element: <Team/>
-  },
-  {
-    path:'/stuff',
-    element: <Stuff/>
-  },
-  {
-    path: '/player/:id',
-    element: <PlayerPage/>
-  },
-  {
-    path: '/stuff/:id',
-    element: <PlayerPage/>
-  },
-  {
-    path:'/history',
-    element: <History/>
-  },
-  {
-    path:'/photos',
-    element: <Photos/>
-  },
-  {
-    path: '/table',
-    element: <Table/>
-  },
-  {
-    path:'/videos',
-    element: <Photos/>
-  },
-
-
-])
-
+    basename: '/Victoria'
+  }
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}>
       <App />
     </RouterProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
